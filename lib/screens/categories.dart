2dcd20fa264d14166976bd24a3eef02ffbb96d96
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-04-17 18:12:41 +0700

    feat: implement meal item widget

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (ctx) => const MealsScreen(title: 'Meals', meals: []),
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
       ),
     );
   }
@@ -34,7 +43,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             )
         ],
