f255ade6e51471a2f98646fc21ce4adb0b068d95
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-04-22 11:01:42 +0700

    feat: add drawer to filters screen

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/screens/tabs.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class FilterScreen extends StatefulWidget {
   const FilterScreen({super.key});
@@ -16,6 +18,17 @@ class _FilterScreenState extends State<FilterScreen> {
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
+      drawer: MainDrawer(onSelectScreen: (identifier) {
+        Navigator.pop(context);
+        if (identifier == 'meals') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (ctx) => const TabsScreen(),
+            ),
+          );
+        }
+      }),
       body: Column(
         children: [
           SwitchListTile(
